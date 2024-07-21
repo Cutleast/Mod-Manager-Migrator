e750a4e8b2ae6adb2888483c5801533f42e292d4
Cutleast <cutleast@gmail.com>
2024-07-21 10:33:41 +0200

    Improve and fix conflict graph

--- src/utilities/conflict_graph.py
+++ src/utilities/conflict_graph.py
@@ -1,12 +1,25 @@
+"""
+This file is part of Mod Manager Migrator
+by Cutleast and falls under the license
+Attribution-NonCommercial-NoDerivatives 4.0 International.
+"""
+
 from .mod import Mod
 
-class Node: #used only internally within ConflictGraph
+
+class Node:
+    """
+    Used only internally within ConflictGraph!
+    """
+
     key: int
     value: Mod
     child_count: int
     parent_count: int
 
-    def __init__(self, key, value, child_count = 0, parent_count = 0) -> None:
+    def __init__(
+        self, key: int, value: Mod, child_count: int = 0, parent_count: int = 0
+    ):
         self.key = key
         self.value = value
         self.child_count = child_count
@@ -14,39 +27,41 @@ class Node: #used only internally within ConflictGraph
 
 
 class ConflictGraph:
-    nodes: set
-    edges: dict
+    """
+    Initialize the graph based on a list of mod objects.
+
+    Args:
+        loadorder (list): arbitrary list of mods
+    """
+
+    nodes: list[Node]
+    edges: dict[int, list[Node]]
     used_flag: bool
 
-    def __init__(self, loaderder: list) -> None:
-        """Initialize the graph based on a list of mod objects.
-
-        Args:
-            loaderder (list): arbitraty list of mods
-        """
-        id = 0
+    def __init__(self, loaderder: list[Mod]):
         self.nodes = []
         self.edges = {}
         self.used_flag = False
-        modname2id = {}
+        modname2id: dict[str, int] = {}
+
         # insert all nodes
-        for mod in loaderder:
-            node = Node(key=id, value = mod, child_count = 0, parent_count = 0)
+        for id, mod in enumerate(loaderder):
+            node = Node(key=id, value=mod)
             self.nodes.append(node)
             modname2id[mod.name] = id
             id += 1
+
         # create all edges
         for node in self.nodes:
             for mod_late in node.value.overwriting_mods:
                 # find its node
                 node_late = self.nodes[modname2id[mod_late.name]]
                 # store the edge
-                if (not node.key in self.edges):
+                if not node.key in self.edges:
                     self.edges[node.key] = []
                 self.edges[node.key].append(node_late)
                 node.child_count += 1
                 node_late.parent_count += 1
-
 
     def to_loadorder(self):
         """
@@ -54,29 +69,31 @@ class ConflictGraph:
 
         Returns:
             A sorted list of mods, based on conflict rules.
-
         """
+
         assert self.used_flag == False
-        new_order = []
-        pop_stack = []
+
+        new_order: list[Mod] = []
+        pop_stack: list[Node] = []
         for node in self.nodes:
-            if(node.parent_count == 0):
+            if node.parent_count == 0:
                 # if it's out of the graph, output it first.
-                if(node.child_count == 0):
+                if node.child_count == 0:
                     new_order.append(node.value)
                 # if it's in the graph and has no parent, get ready to pop it.
                 else:
                     pop_stack.append(node)
-        # remove nodes from the graph 
-        while(len(pop_stack) > 0):
+
+        # remove nodes from the graph
+        while len(pop_stack) > 0:
             node = pop_stack.pop()
-            if (node.key in self.edges):
+            if node.key in self.edges:
                 for child in self.edges[node.key]:
                     child.parent_count -= 1
-                    if (child.parent_count == 0):
+                    if child.parent_count == 0:
                         pop_stack.append(child)
             new_order.append(node.value)
+
         self.used_flag = True
+
         return new_order
-
-
