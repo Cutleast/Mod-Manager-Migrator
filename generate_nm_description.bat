721ba0cdeab71b1248cc4f6dae5dfa14f6902ca6
Cutleast <cutleast@gmail.com>
2025-05-08 21:39:05 +0200

    Add Nexus Mods description

---
+++ generate_nm_description.bat
@@ -0,0 +1,3 @@
+@REM NOT WORKING AT THE MOMENT
+@echo off
+uv run scripts\generate_nm_description.py --file README.md --domain https://raw.githubusercontent.com/Cutleast/Mod-Manager-Migrator/main/ --out-file misc_assets/nm_description.bbc
