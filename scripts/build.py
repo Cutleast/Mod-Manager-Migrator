5574eae72833407234f35f7a04878c30a38c4da8
Cutleast <cutleast@gmail.com>
2025-08-22 19:44:28 +0200

    scripts/build.py: Remove unused file

--- scripts/build.py
+++ scripts/build.py
@@ -77,6 +77,7 @@ if __name__ == "__main__":
             Path("lib") / "PySide6" / "QtOpenGL.pyd",
             Path("lib") / "PySide6" / "QtOpenGLWidgets.pyd",
             Path("lib") / "libcrypto-3.dll",
+            Path("lib") / "mfc140u.dll",
         ],
     )
     backend = Backend()
