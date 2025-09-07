326c9bcc795b600b221d39bf18a7e0c13f5146f1
Cutleast <cutleast@gmail.com>
2025-09-07 13:52:26 +0200

    Fix loadorder sorting when referenced mod is not installed

--- tests/core/instance/test_instance.py
+++ tests/core/instance/test_instance.py
@@ -81,3 +81,38 @@ class TestInstance(BaseTest):
         # then
         assert loadorder.index(overwritten_mod) == instance.mods.index(overwritten_mod)
         assert loadorder.index(overwriting_mod) == instance.mods.index(overwriting_mod)
+
+    def test_get_loadorder_with_missing_mod(self, instance: Instance) -> None:
+        """
+        Tests `core.instance.instance.Instance.get_loadorder` with a mod that is
+        referenced by a mod conflict but missing in the instance.
+        """
+
+        # given
+        instance.order_matters = True
+        overwritten_mod: Mod = self.get_mod_by_name(
+            "Obsidian Weathers and Seasons", instance
+        )
+        overwriting_mod: Mod = self.get_mod_by_name(
+            "Obsidian Weathers and Seasons - German", instance
+        )
+
+        # then
+        assert overwritten_mod.mod_conflicts == [overwriting_mod]
+
+        # when
+        instance.mods.remove(overwriting_mod)
+
+        # then (mod conflict should be unaffected)
+        assert overwritten_mod.mod_conflicts == [overwriting_mod]
+
+        # when
+        loadorder: list[Mod] = instance.get_loadorder(False)
+        sorted_index: int = loadorder.index(overwritten_mod)
+        original_index: int = (
+            # subtract 1 for removed mod
+            instance.mods.index(overwritten_mod) - 1
+        )
+
+        # then
+        assert sorted_index == original_index
